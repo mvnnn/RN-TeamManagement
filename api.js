af8d6a46064b86ba94db7f88f3d8108c84b557df
Bhautik Mavani <mavanibhautik@gmail.com>
2017-06-17 16:27:59 +0530

    update memebers data

--- api.js
+++ api.js
@@ -1,7 +1,8 @@
 const member = [
-  { first_name: 'Adien', last_name:'Olczak(admin)', email:'adrien@instawork.com', mobile_no:"415-310-1619", role:0 },
-  { first_name: 'Adien2', last_name:'Olczak(admin)', email:'adrien@instawork.com', mobile_no:"415-310-1619", role:1 },
-  { first_name: 'Adien3', last_name:'Olczak(admin)', email:'adrien@instawork.com', mobile_no:"415-310-1619",role:0 }
+  { first_name: 'Adien', last_name:'Olczak(admin)', email:'adrien@instawork.com', mobile_no:"415-310-1619", role:1 },
+  { first_name: 'Charlene', last_name:'Pham', email:'charlene@instawork.com', mobile_no:"415-310-1619", role:0 },
+  { first_name: 'Benson', last_name:'Mach', email:'benson@instawork.com', mobile_no:"415-310-1619",role:0 },
+  { first_name: 'Dan', last_name:'Petrie', email:'dan@instawork.com', mobile_no:"415-310-1619",role:0 }
 ]
 
 export default () => {
